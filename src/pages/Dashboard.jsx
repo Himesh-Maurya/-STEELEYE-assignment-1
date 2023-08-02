c5a207a1e31720a6cae6f4565d6f52010a238dbc
Himesh <himeshmauurya@gmail.com>
2023-08-02 20:03:12 +0530

    displaying the order number

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+// Data
+import mockData from "../assets/data.json";
+import timestamps from "../assets/timeStamps.json";
+
+// Components
+import Dropdown from "../component/dropdown/Dropdown";
+import HeaderTitle from "../component/header-title/HeaderTitle";
+import Search from "../component/search/Search";
+import List from "../component/list/List";
+
+// Styles
+import styles from "./Dashboard.module.css";
+import Card from "../component/card/Card";
+//console.log(mockData.results.length)
+const Dashboard = () => {
+  const [currency, setCurrency] = useState("EUR");
+  const [searchText, setSearchText] = useState("");
+  const [selectedOrderDetails, setSelectedOrderDetails] = useState({});
+  const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({});
+
+  return (
+    <div>
+      <div className={styles.header}>
+      {/* displaying the correct numbers of order */}
+        <HeaderTitle primaryTitle="Orders" secondaryTitle= {`${mockData.results.length} order`}/>
+        <div className={styles.actionBox}>
+          <Search
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Dropdown
+            options={["GBP", "USD", "JPY", "EUR"]}
+            onChange={(e) => setCurrency(e.target.value)}
+            selectedItem={currency}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.section}>
+          <Card
+            cardData={selectedOrderDetails}
+            title="Selected Order Details"
+          />
+          <Card
+            cardData={selectedOrderTimeStamps}
+            title="Selected Order Timestamps"
+          />
+        </div>
+        <List rows={mockData.results}/>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
